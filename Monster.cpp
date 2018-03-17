df428fb5ee8b4a27eaa3dc49d08626f962af0cd1
Seo Hyeon Myeong <37466204+SeoHyoenMyeong@users.noreply.github.com>
2018-03-17 17:32:43 +0900

    Create Monster.cpp
    몬스터 클래스 생성

---
+++ Monster.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+void Monster{
+	int HP;                 //체력
+	int Max_HP;             //최대체력
+	int MP;                 //마나
+	int Max_MP;             //최대마나
+	char name[10];          //이름
+	float damage;           //공격력
+	float masic_power;      //주문력
+	int armor;              //방어력
+	int masic_resistance;   //마법저항력
+	int exp_feeding;        //경험치
+	int level;              //레벨
+	float agility;          //공격속도
+	float utility;          //이동속도
+    
+    void Monster(char *name){
+	   	Player.HP = ;
+		Player.Max_HP = 50;
+		Player.MP = 0;
+		Player.Max_MP = 0;
+		Player.name = name;
+		Player.damage = 1.0;
+		Player.masic_power = 0;
+		Player.armor = 0 ;
+		Player.masic_resistance = 0;
+		Player.exp_feeding = 0;
+		Player.level = 1;
+		Player.agility = 0.5;
+		Player.utility = 300.0;
+	}
+}
