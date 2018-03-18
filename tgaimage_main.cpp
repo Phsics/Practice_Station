ff364348002c9b5cbe6821c592e3152a40760cbc
Seo Hyeon Myeong <37466204+SeoHyoenMyeong@users.noreply.github.com>
2018-03-18 14:11:06 +0900

    Update tgaimage_main.cpp

--- tgaimage_main.cpp
+++ tgaimage_main.cpp
@@ -4,8 +4,8 @@ const TGAColor white = TGAColor(255, 255, 255, 255);        //  컬러를 지정
 const TGAColor red   = TGAColor(255, 0,   0,   255);        
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {	// 선그리기 (x0,y0) ~ (x1,y1) 사이에 color  칠하기 
-    for (float t=0.; t<1.; t+=.1) {
-        int x = x0*(1.-t) + x1*t;
+	for (int x=x0; x<=x1; x++) {
+        float t = (x-x0)/(float)(x1-x0);
         int y = y0*(1.-t) + y1*t;
         image.set(x, y, color);
     }
