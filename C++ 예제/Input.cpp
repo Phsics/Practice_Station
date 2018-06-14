c8809f05f4f956404694dad8ee9a43b2e8219d7c
Seo Hyeon Myeong <37466204+SeoHyoenMyeong@users.noreply.github.com>
2018-06-14 19:13:41 +0900

    Create Input.cpp

---
+++ C++ 예제/Input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void){
+
+	int nData;
+	
+	std::cout <<"Please input a number : ";
+	std::cin >>nData;
+	std::cout << "Number : " << nData << std::endl;
+	
+	return 0;
+}
