0bd902f1c87225682a9c7701515aa73a41a39a33
Seo Hyeon Myeong <37466204+SeoHyoenMyeong@users.noreply.github.com>
2018-06-14 18:58:33 +0900

    Create Varience.cpp

---
+++ C++ 예제/Varience.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(void)
+{
+	int nData = 10;
+	int nData2;
+	char cData = 'a';
+	float fData = 1.50f;
+	double dData = 1.50;
+	
+	nData2 = 20;
+	
+	std::cout << nData << " " << nData2 << std::endl;
+	std::cout << cData << std::endl;
+	std::cout << fData << std::endl;
+	std::cout << dData << std::endl;
+	
+	return 0;
+}
