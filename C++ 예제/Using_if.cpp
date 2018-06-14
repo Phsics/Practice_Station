0a6e8baed5627ff070d01481bcbfd1c5d5b02d7d
Seo Hyeon Myeong <37466204+SeoHyoenMyeong@users.noreply.github.com>
2018-06-14 19:20:24 +0900

    Create Using_if.cpp

---
+++ C++ 예제/Using_if.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(void){
+
+	int nData = 10;
+	
+	if (nData == 10){
+		std::cout << "right" <<std::endl;
+	}	
+	else {
+		std::cout << "wrong" <<std::endl;
+	}
+	return 0;
+}
