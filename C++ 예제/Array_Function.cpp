4f77f88664a1e034ff497876420319791a0cd43e
Seo Hyeon Myeong <37466204+SeoHyoenMyeong@users.noreply.github.com>
2018-06-16 13:18:23 +0900

    Update Array_Function.cpp

--- C++ 예제/Array_Function.cpp
+++ C++ 예제/Array_Function.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 
-void printData(int * data) {
-    int n = sizeof(data)/sizeof(*data);
-    for(int i=0;i<n;i++){
-        std:cout << "데이터 " << i+1 << "번 : " << data[i] << std::endl;
+void printData(int * data,int count) {
+	
+    for(int i=0;i<count;i++){
+        std::cout << "데이터 " << i+1 << "번 : " << data[i] << std::endl;
     }
 }
 
 int main(void) {
 
     int data[5] = {10,20,30,40,50};
+    printData(data,sizeof(data)/sizeof(int));
     return 0;
 }
