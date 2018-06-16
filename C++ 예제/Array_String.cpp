52399ef3e414a1badc5115b124f152b17a38e873
Seo Hyeon Myeong <37466204+SeoHyoenMyeong@users.noreply.github.com>
2018-06-16 09:44:38 +0900

    Create Array_String.cpp

---
+++ C++ 예제/Array_String.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+int main(void) {
+	
+	char string1[6] = "Hello";
+	char string2[5] = "Good";
+	char string3[8] = "Morning";
+	
+	std::cout << string1 << std::endl;
+	std::cout << string2 << " " << string3 << std::endl;
+
+	return 0 ;
+}
